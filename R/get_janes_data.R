378f9142fdf6802bfd301c645660bcdbaeb4598a
Chad Peltier <chad.peltier@ihsmarkit.com>
2020-07-27 10:40:17 -0400

    added function for get_janes_data. Required for all "get_janes_..." functions

---
+++ R/get_janes_data.R
@@ -0,0 +1,25 @@
+#' @title get_janes_data
+#' @description Pulls Janes data for all data endpoints. Helper function.
+#'
+#' @param x For iteration on URLs
+#'
+#' @return Janes data. Used in all "get_janes_x" functions.
+#' @importFrom httr GET
+#' @importFrom httr add_headers
+#' @importFrom httr content
+#' @importFrom jsonlite fromJSON
+#' @importFrom magrittr "%>%"
+#' @importFrom tibble tibble
+
+
+
+get_janes_data <- function(x){
+  request <- GET(url = x, add_headers(Authorization = janes_key))
+  response <- content(request, as = "text")
+  response %>%
+    fromJSON() %>%
+    tibble()
+}
+
+#' @export
+
