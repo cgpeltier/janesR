8a7b02c568ad5cc9dff4f1942fe5839fa3b165f7
cgpeltier <cgpeltier@gmail.com>
2021-02-26 11:00:45 -0500

    added additional arguments for filters

--- R/get_janes.R
+++ R/get_janes.R
@@ -11,6 +11,7 @@
 #' @importFrom httr content
 #' @importFrom jsonlite fromJSON
 #' @importFrom stringr str_replace_all
+#' @importFrom stringr str_replace
 #' @importFrom magrittr "%>%"
 #' @importFrom stringr str_remove
 #' @importFrom purrr map
@@ -40,7 +41,11 @@
 # support for queries
 
 
+
+
 get_janes <- function(country = NULL, parallel = FALSE,
+                      post_date = NULL, include_del = FALSE,
+                      source_type = NULL,
                       endpoint = c("inventories", "equipment", "orbats",
                                    "bases", "airports", "countryrisks",
                                    "companies", "events", "equipmentrelationships",
@@ -49,12 +54,15 @@ get_janes <- function(country = NULL, parallel = FALSE,
                                    "defenceprogrammes", "references", "news")) {
 
   countries <- paste0(country, collapse = ")%3Cor%3Ecountryiso(")
+  post_dates <- str_replace(post_date, "::", "%3A%3A")
+  sources <- str_replace_all(source_type, " ", "%20")
 
   if(endpoint %in% c("references", "news")) {
       ## to save all XMLs
 
       request_url <- paste0("https://developer.janes.com/api/v1/", endpoint,
-                          "?f=countryiso(", countries, ")&num=100000")
+                          "?f=countryiso(", countries, ")%3Cand%3EpostDate(",
+                          post_dates, ")&num=100000&includeDeleted=", include_del)
 
 
       if(parallel == FALSE){
@@ -89,11 +97,64 @@ get_janes <- function(country = NULL, parallel = FALSE,
 
 
 
+  } else if(endpoint == "events") {
+
+    request_url <- paste0("https://developer.janes.com/api/v1/data/", endpoint,
+                          "?f=countryiso(", countries, ")%3Cand%3EpostDate(",
+                          post_dates, ")%3Cand%3Esource_type(", sources,
+                          ")&num=100000&includeDeleted=", include_del)
+
+    if(parallel == FALSE){
+
+      GET(request_url, add_headers("Authorization" = Sys.getenv("JANES_KEY"))) %>%
+        content(as = "text") %>%
+        fromJSON() %>%
+        pluck(2) %>%
+        tibble() %>%
+        dplyr::pull(url) %>%
+        map(~ GET(.x,  add_headers("Authorization" = Sys.getenv("JANES_KEY"))) %>%
+              content(as = "text") %>%
+              fromJSON() %>%
+              tibble())  %>%
+        tibble() %>%
+        conditional_unnest_wider(".") %>%
+        conditional_unnest_wider(".") %>%
+        unnest_all2() %>%
+        unnest_all2() %>%
+        unnest_all2() %>%
+        unnest_all2() %>%
+        unnest_all2()
+  } else {
+
+     GET(request_url, add_headers("Authorization" = Sys.getenv("JANES_KEY"))) %>%
+       content(as = "text") %>%
+       fromJSON() %>%
+       pluck(2) %>%
+       tibble() %>%
+       dplyr::pull(url) %>%
+       future_map(~ GET(.x,  add_headers("Authorization" = Sys.getenv("JANES_KEY"))) %>%
+             content(as = "text") %>%
+             fromJSON() %>%
+             tibble())  %>%
+       tibble() %>%
+       conditional_unnest_wider(".") %>%
+       conditional_unnest_wider(".") %>%
+       unnest_all2() %>%
+       unnest_all2() %>%
+       unnest_all2() %>%
+       unnest_all2() %>%
+       unnest_all2()
+   }
+
+
+
     } else {
       ## for all JSON data
 
       request_url <- paste0("https://developer.janes.com/api/v1/data/", endpoint,
-                                "?f=countryiso(", countries, ")&num=100000")
+                            "?f=countryiso(", countries, ")%3Cand%3EpostDate(",
+                            post_dates, ")%3Cand%3Esource_type(", sources,
+                            ")&num=100000&includeDeleted=", include_del)
 
 
       if(parallel == FALSE){
